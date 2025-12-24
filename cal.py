6680b65f1927c56bb335128403555f25a975ad5a
noorulaink2008-beep <noorulain.k.2008@gmail.com>
2025-12-24 14:55:35 +0500

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,14 @@
+a = float(input("Enter first number"))
+b = float(input("Enter second number"))
+print("1.Add\n2.Subtract\n3.Multiply\n4.Divide")
+choice = int(input("Choose options"))
+if choice == 1:
+	print("Result =", a + b)
+elif choice == 2:
+	print("Result =", a - b)
+elif choice == 3:
+	print("Result =", a * b)
+elif choice == 4:
+	print("Result =", a / b)
+else:
+	print("Wrong")
